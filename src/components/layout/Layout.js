7dd96c6dff5b43188f7c6d5e53c7b4e14faae924
Wayne Pascoe <wayne+github@penguinpowered.org>
2020-02-15 21:17:41 +0000

    Reuse helmet

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -11,41 +11,7 @@ const Layout = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
     <div>
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
-
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
-      </Helmet>
+      <LayoutHelmet />
       <Header />
       <main className="text-gray-900">{children}</main>
       <Footer />
@@ -59,6 +25,19 @@ export const SubLayout = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
     <div>
+      <LayoutHelmet />
+      <SubHeader />
+      <main className="text-gray-900">{children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+// export const SubLayout
+
+export const LayoutHelmet = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+  return (
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
@@ -94,11 +73,5 @@ export const SubLayout = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
       </Helmet>
-      <SubHeader />
-      <main className="text-gray-900">{children}</main>
-      <Footer />
-    </div>
   )
 }
-
-// export const SubLayout
