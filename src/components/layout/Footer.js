90787b20dbea38aac4683f3f77f5f7cd4f558b61
Jonathan Meyers <jonathan@cybrary.it>
2020-02-15 18:13:21 +0000

    text

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => (
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">About Us</h2>
-        <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
+        <p className="mt-5">DoneOps was founded by a group of seasoned DevOps personnel.</p>
       </div>
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Important Links</h2>
@@ -29,48 +29,48 @@ const Footer = () => (
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <div style={{float:'left', width: '10%'}}>
+            <div style={{ float: 'left', width: '10%' }}>
               <img src={linkedin} alt="LinkedIn" style={{ width: '2em', height: '2em' }} />
             </div>
-            <div style={{float:'right', width: '90%'}}>
+            <div style={{ float: 'right', width: '90%' }}>
               <a title="LinkedIn" href="https://www.linkedin.com/company/doneops">
                 LinkedIn
               </a>
             </div>
-            <div style={{clear:'both'}}></div>
+            <div style={{ clear: 'both' }}></div>
           </li>
           <li>
-            <div style={{float:'left', width: '10%'}}>
+            <div style={{ float: 'left', width: '10%' }}>
               <img src={youtube} alt="YouTube" style={{ width: '2em', height: '2em' }} />
             </div>
-            <div style={{float:'right', width: '90%'}}>
+            <div style={{ float: 'right', width: '90%' }}>
               <a title="YouTube" href="https://www.youtube.com/channel/UC0sAsIUdK5fsHnNQlKhLzWQ">
                 YouTube
               </a>
             </div>
-            <div style={{clear:'both'}}></div>
+            <div style={{ clear: 'both' }}></div>
           </li>
           <li>
-            <div style={{float:'left', width: '10%'}}>
+            <div style={{ float: 'left', width: '10%' }}>
               <img src={github} alt="GitHub" style={{ width: '2em', height: '2em' }} />
             </div>
-            <div style={{float:'right', width: '90%'}}>
+            <div style={{ float: 'right', width: '90%' }}>
               <a title="GitHub" href="https://github.com/doneops">
                 GitHub
               </a>
             </div>
-            <div style={{clear:'both'}}></div>
+            <div style={{ clear: 'both' }}></div>
           </li>
           <li>
-            <div style={{float:'left', width: '10%'}}>
+            <div style={{ float: 'left', width: '10%' }}>
               <img src={twitter} alt="Twitter" style={{ width: '2em', height: '2em' }} />
             </div>
-            <div style={{float:'right', width: '90%'}}>
+            <div style={{ float: 'right', width: '90%' }}>
               <a title="Twitter" href="https://twitter.com/doneops">
                 Twitter
               </a>
             </div>
-            <div style={{clear:'both'}}></div>
+            <div style={{ clear: 'both' }}></div>
           </li>
           {/* <li>
             <img src={instagram} alt="Instagram" style={{ width: '1em', height: '1em' }} />
