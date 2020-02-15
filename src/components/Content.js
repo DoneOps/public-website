7b0d7319e774c13c2bf9199433e6fdffced37a71
Jonathan Meyers <jonathan@cybrary.it>
2020-02-15 15:22:52 +0000

    format

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,9 +5,7 @@ export const HTMLContent = ({ content, className }) => (
   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
 )
 
-const Content = ({ content, className }) => (
-  <div className={className}>{content}</div>
-)
+const Content = ({ content, className }) => <div className={className}>{content}</div>
 
 Content.propTypes = {
   content: PropTypes.node,
