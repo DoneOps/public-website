ae381b3ca84ee9ddd0869c6232c3508d270fbc09
Jonathan Meyers <jonathan@cybrary.it>
2020-02-15 16:01:25 +0000

    new pages

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const PrivacyPolicyPage = () => (
+  <Layout>
+    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
+      <h3 className="text-5xl font-semibold">Privacy Policy</h3>
+    </section>
+  </Layout>
+)
+
+export default PrivacyPolicyPage
