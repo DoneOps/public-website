1c19cfef86788e401b8515f40402b35bb4206b7f
Jonathan Meyers <jonathan@cybrary.it>
2020-03-23 12:05:45 +0800

    we

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
       resolve: 'gatsby-plugin-sri',
       options: {
         hash: 'sha512', // 'sha256', 'sha384' or 'sha512' ('sha512' = default)
-        crossorigin: false,
+        crossorigin: true,
       },
     },
     'gatsby-transformer-sharp',
