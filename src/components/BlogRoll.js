2da2510da9c70fb5f6288d0eeaf146b2fcc3572f
Wayne P <wayne@penguinpowered.org>
2022-04-28 20:28:44 +0100

    fix: Remove bullet point in blog entris on blog index

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -38,7 +38,7 @@ class BlogRoll extends React.Component {
                     >
                       {post.frontmatter.title}
                     </Link>
-                    <span> &bull; </span>
+                    <span> &nbsp; </span>
                     <span className='subtitle is-size-5 is-block'>
                       {post.frontmatter.date}
                     </span>
