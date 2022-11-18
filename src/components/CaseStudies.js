b971d0593524d31a805432f3a910c7a311733d3b
Jonathan Meyers <jmymy@users.noreply.github.com>
2022-11-18 10:24:08 -0600

    fix(deps): package updates (#360)
    * fix(deps): package updates
    
    * fix(deps): bump yarn and node
    
    * chore: dep bumps

--- src/components/CaseStudies.js
+++ src/components/CaseStudies.js
@@ -73,8 +73,8 @@ export default () => (
     query={graphql`
       query CaseStudiesQuery {
         allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "case-study" } } }
+          sort: {frontmatter: {date: DESC}}
+          filter: {frontmatter: {templateKey: {eq: "case-study"}}}
         ) {
           edges {
             node {
